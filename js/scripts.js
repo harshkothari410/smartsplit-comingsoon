3d96059fc9753d0d303c18be1669930a2f8f0f61
Harsh Kothari <harshkothari410@gmail.com>
2016-11-23 00:46:36 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(window).load(function() {
 
 $(document).ready(function(){
   $("#counter").countdown({
-  until: new Date(2016, 5 - 1, 15),
+  until: new Date(2016, 12 - 1, 5),
   format: 'dHMS'
   });
 
